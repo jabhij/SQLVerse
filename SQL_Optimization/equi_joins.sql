61fd3411825ea2def90733000813dc5f20efdd2f
Abhishek Jaiswal <jaisabhii@gmail.com>
2022-09-17 11:34:40 -0500

    Update equi_joins.sql

--- SQL_Optimization/equi_joins.sql
+++ SQL_Optimization/equi_joins.sql
@@ -1,17 +1,37 @@
 /*
-
+Use simple EQUI JOINS
 */
 
 
 /*
-
+Using 
 */
 
+SELECT 
+  *
+FROM 
+  table1 a
+JOIN
+  table2 b
+ON a.date = CONCAT(b.year, '-', b.month, '-', b.day)
 
 /*
-
+Using simple Equi Joins
 */
 
+SELECT 
+  *
+FROM 
+  table1 a
+JOIN (
+  SELECT
+    name, CONCAT(b.year, '-', b.month, '-', b.day) as date
+  FROM 
+    table2 b
+    ) new
+
+ON a.date = new.date
+    
 
 /*
 Case Study
