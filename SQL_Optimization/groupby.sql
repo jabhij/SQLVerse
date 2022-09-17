b14bd1a3b736d79437a4b0eaa7f75fb0eb25ea72
Abhishek Jaiswal <jaisabhii@gmail.com>
2022-09-17 11:39:45 -0500

    Update groupby.sql

--- SQL_Optimization/groupby.sql
+++ SQL_Optimization/groupby.sql
@@ -1,17 +1,35 @@
 /*
-
+Always 'GROUP BY' the attribute/column with the largest number of unique entities/values 
 */
 
 
 /*
-
+Demo this- NO
 */
-
+SELECT
+  main_category,
+  sub_category,
+  item_id,
+  SUM(price)
+  
+FROM
+  table1
+GROUP BY
+  main_category, sub_category, item_id
 
 /*
-
+Demo this- YES
 */
-
+SELECT
+  main_category,
+  sub_category,
+  item_id,
+  SUM(price)
+  
+FROM
+  table1
+GROUP BY
+  item_id, sub_category, main_category
 
 /*
 Case Study
