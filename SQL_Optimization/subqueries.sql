9df88cccca6c5da578a19a0c60b7da352f44b41e
Abhishek Jaiswal <jaisabhii@gmail.com>
2022-09-17 11:45:38 -0500

    Update subqueries.sql

--- SQL_Optimization/subqueries.sql
+++ SQL_Optimization/subqueries.sql
@@ -1,17 +1,36 @@
 /*
-
+Avoid subqueries in WHERE Clause
 */
 
 
 /*
-
+Subqueries in WHERE Clause
 */
-
+SELECT 
+  SUM(price)
+FROM
+  table1 
+WHERE
+  itemid IN (
+    SELECT itemid
+    FROM table2
+)
 
 /*
-
+Avoiding subqueries in WHERE Clause
 */
-
+WITH t2 AS(
+  SELECT itemid
+  FROM table2
+)
+SELECT 
+  SUM(price)
+FROM
+  table1 as t1
+JOIN
+  t2
+ON t1.itemid = t2.itemid
+)
 
 /*
 Case Study
