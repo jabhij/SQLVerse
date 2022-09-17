6a0530820034feeef5c2a1bde5d2b18cf8f6e705
Abhishek Jaiswal <jaisabhii@gmail.com>
2022-09-17 10:40:57 -0500

    Create TemporaryFIle.sql

---
+++ SQL_Optimization/TemporaryFIle.sql
@@ -0,0 +1,42 @@
+/*
+Converting long list of IN clause into a temporary table
+*/
+
+
+/*
+IN Clause 
+*/
+
+SELECT *
+FROM table1 as T1
+WHERE
+  itemid IN (123453, 535647, 123486, 954524, ..., 348592)
+
+/*
+Temporary Table
+*/
+
+SELECT * 
+FROM table1 as T1
+JOIN (
+  SELECT 
+    itemid 
+  FROM (
+    SELECT 
+      Split ('123453, 535647, ...', '123486, 954524, ...')
+          as bar
+     )
+     
+  CROSS JOIN
+    UNNEST(bar) AS t(itemid)
+) AS table2 as T2
+
+ON
+    T1.itemid = T2.itemid
+
+        
+/*
+Case Study
+*/
+
+??
