4e790be3053e45ca009bd211e83e25e86576f140
Abhishek Jaiswal <jaisabhii@gmail.com>
2022-09-17 11:50:15 -0500

    Update max_function.sql

--- SQL_Optimization/max_function.sql
+++ SQL_Optimization/max_function.sql
@@ -1,17 +1,30 @@
 /*
-
+Use MAX instead of RANK
 */
 
 
 /*
-
+Use of MAX
 */
-
+SELECT
+  *
+FROM (
+  SELECT
+      userid,
+      RANK() over (ORDER BY prdate desc) AS rank
+  FROM table1
+)
+WHERE ranking = 1
 
 /*
-
+Use of RANK
 */
+SELECT
+  userid, MAX(prdate)
+FROM
+  table1
 
+GROUP BY 1
 
 /*
 Case Study
