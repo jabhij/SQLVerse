fa6ef489b2cb30563e84ee3aa5d41978f1298ca7
Abhishek Jaiswal <jaisabhii@gmail.com>
2022-09-19 22:33:16 -0500

    Update approx_distinct.sql

--- SQL_Optimization/approx_distinct.sql
+++ SQL_Optimization/approx_distinct.sql
@@ -5,14 +5,18 @@ Use APPROX_DISTINCT() instead of COUNT(distinct) for very large datasets
 /*
 Use of COUNT(distinct)
 */
-
+SELECT 
+  COUNT(distinct col_name)
+FROM
+  table1
 
 /*
 Use of APPROX_DISTINCT()
 */
-SELECT APPROX_DISTINCT(close_value)
+SELECT 
+  APPROX_DISTINCT(close_value)
 FROM
-  sales_pipeline
+  table1
   
 /*
 Case Study
