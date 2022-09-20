dfa7eee1526f76254425e878fc6514be04c8172c
Abhishek Jaiswal <jaisabhii@gmail.com>
2022-09-19 22:36:21 -0500

    Update approx_percentile.sql

--- SQL_Optimization/approx_percentile.sql
+++ SQL_Optimization/approx_percentile.sql
@@ -1,14 +1,14 @@
 /*
-Use approx_percentile(metric, 0.5) for median
+Use APPROX_PERCENTILE(close_value, 0.5) for median
 */
 
 /*
-
+Use of APPROX_PERCENTILE()
 */
-
-/*
-
-*/
+SELECT 
+  APPROX_PERCENTILE(close_value, 0.5)
+FROM 
+  table1
 
 /*
 Case Study
