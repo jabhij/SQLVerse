6a961c5e0b4785660296a566cf4ed02e0e965dc7
Abhishek Jaiswal <jaisabhii@gmail.com>
2022-10-08 14:45:23 -0500

    Update Count.sql

--- 60DaysofSQL/Count.sql
+++ 60DaysofSQL/Count.sql
@@ -7,21 +7,56 @@ count(string) - counts all the values in that column, including NULL(s)
 
 */
 
-
+Table1 -
+------------------------------
+Col_1 | Col_2 | Col_3 | result 
+------------------------------
+   A  |   1   |   Z   |   0
+------------------------------
+   B  |   2   |  NULL |   9
+------------------------------
+   C  |   3   |   X   |   8
+------------------------------
+   D  |  NULL |   W   |   7
+------------------------------
 
 
 
 SELECT
 COUNT(*) FROM Table1
+AS result
+------
+result  
+------
+  4   
+-----
 
 
 SELECT
-COUNT() FROM Table1
+COUNT(col_2) FROM Table1
+AS result
+------
+result  
+------
+  5   
+-----
 
 
 SELECT
-COUNT(*) FROM Table1
+COUNT(1) FROM Table1
+AS result
+------
+result  
+------
+  5   
+-----
 
 
 SELECT
-COUNT(*) FROM Table1
+COUNT(One) FROM Table1
+AS result
+------
+result  
+------
+  5   
+-----
