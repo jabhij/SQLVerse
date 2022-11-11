73f432b09d65d375871039d6f6b80694f2dc8941
Abhishek Jaiswal <jaisabhii@gmail.com>
2022-11-11 16:18:04 -0600

    Update 10.2. MultipleRowSubQ.sql

--- Basics/10.2. MultipleRowSubQ.sql
+++ Basics/10.2. MultipleRowSubQ.sql
@@ -29,9 +29,23 @@ Students --                                                   University_Details
 +------------+--------------+--------------+--------+         +------------+--------------+         +------------+-----------+--------+   
 |      4     |       D      |     S2025    |   3.0  |         |      5     |      XYU     |         |     IJU    |    ARCH   |    2   |  
 +------------+--------------+--------------+--------+         +------------+--------------+         +------------+-----------+--------+   
-|      5     |       E      |     NULL     |   2.5  |         |      4     |      IJU     |         |     JKU    |    BS     |    1   |  
+|      5     |       E      |       -      |   2.5  |         |      4     |      IJU     |         |     JKU    |    BS     |    1   |  
 +------------+--------------+--------------+--------+         +------------+--------------+         +------------+-----------+--------+   
                                                                                                     |     LMN    |    ML     |    3   | 
                                                                                                     +------------+-----------+--------+ 
                                                                                                     
 
+-- Multiple columns and multiple rows --
+
+
+
+-- One column and multiple rows -- ??
+/* Select the University name where a student's passing year is unkown/missing */ -- ??
+SELECT Student_Name
+  FROM Students
+    WHERE Passing_year NOT IN (SELECT Uni_Name FROM University_Details);
+    
+/* Select the Course for which grades are below 3.0 */ -- ??
+SELECT Course
+  FROM Ranks
+    WHERE 
