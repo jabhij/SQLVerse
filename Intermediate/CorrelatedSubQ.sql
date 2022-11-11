59c657a55b3d8e99a72a4a9f387964325d505db1
Abhishek Jaiswal <jaisabhii@gmail.com>
2022-11-11 13:58:51 -0600

    Update CorrelatedSubQ.sql

--- Intermediate/CorrelatedSubQ.sql
+++ Intermediate/CorrelatedSubQ.sql
@@ -1,14 +1,14 @@
 Correlated Subqueries --
-+-------------------------------------------------------------------+-------------------------------------------+
-|                             DETAILS                               |                   DEMO                    |
-+-------------------------------------------------------------------+-------------------------------------------+
-| - Correlated subqueries are subqueries that reference one or more | SELECT ---                                |
-|   columns in the main query.                                      |   --- Subquery goes in here ---           |
-| - Correlated subqueries depend on information in the main query   |   FROM ---                                |
-|   to run, and thus, cannot be executed on their own.              |
-| - Correlated subqueries are evaluated in SQL once per row of data |
-|   retrieved -- a process that takes a lot more computing power &  | 
-|   time than a simple subquery.                                    |
++------------------------------------------------------------------------------+-------------------------------------------+
+|                                 DETAILS                                      |                   DEMO                    |
++------------------------------------------------------------------------------+-------------------------------------------+
+| - Correlated subqueries are subqueries that reference one or more columns in | SELECT ---                                |
+|   in the main query.                                                         |   --- Subquery goes in here ---           |
+| - Correlated subqueries depend on information in the main query to run, and  |   FROM ---                                |
+|   thus, cannot be executed on their own.                                     |                     
+| - Correlated subqueries are evaluated in SQL once per row of data retrieved  |
+|   -- a process that takes a lot more computing power & time than a simple    | 
+|   subquery.                                                                  |
 +-------------------------------------------------------------------------------------------------------------+
 
  
