6730640b6ccfb7dc799c4c6db102a832b6d2a7ba
Abhishek <jaisabhii@gmail.com>
2024-10-12 20:39:06 -0500

    Update 7. Joins_vs_Union.sql

--- Basics/7. Joins_vs_Union.sql
+++ Basics/7. Joins_vs_Union.sql
@@ -6,5 +6,19 @@
 | - Two tables can have different number of columns, but  | - Two tables must have same number of columns, and the  |
 |   the corresponding columns must have compatible types. |   corresponding columns must have compatible types.     |
 +---------------------------------------------------------+---------------------------------------------------------+
-| - 
-+---------------------------------------------------------+---------------------------------------------------------+
+
+  
++---------------+-------------------------------------------------+-------------------------------------------------+
+|     Aspect	  |                    FULL JOIN                    |                       UNION                     |
++---------------+-------------------------------------------------+-------------------------------------------------+
+| Combines Data | - Horizontally (joins columns)                  | - Vertically (stacks rows)                      |
++---------------+-------------------------------------------------+-------------------------------------------------+
+|   Duplicates	| - Keeps all data, including unmatched rows      | - Removes by default (use UNION ALL to keep     |
+|               |   filled with NULLs                             |   duplicates)                                   |
++---------------+-------------------------------------------------+-------------------------------------------------+
+|    Use Case   | - Merge tables to see data relationships,       | - Combine similar results from two tables       |
+|               |   including unmatched rows                      |                                                 |
++---------------+-------------------------------------------------+-------------------------------------------------+
+|   Required    | - Can have different columns and data types in  | - Same number of columns and compatible data    |
+|   Columns     |   each table                                    |   types                                         |
++---------------+-------------------------------------------------+-------------------------------------------------+
